03832f50dfe2393b2fdacdec11486da34e7f8973
Ryan Yocum <rtyocum1@gmail.com>
2020-12-21 15:23:28 -0500

    Allowed Gate Keeper in all channels

--- src/commands/portal/portal.js
+++ src/commands/portal/portal.js
@@ -4,10 +4,13 @@ async function run(dc, message, args) {
   const portal = dc.channels.cache.get(dc.state.get('portal').portalId);
   const private = dc.channels.cache.get(dc.state.get('portal').privateId);
   const privateChat = dc.channels.cache.get(dc.state.get('portal').privateChatId);
-  if (!message.member.permissionsIn(privateChat).has(1024)) return;
+  if (!message.member.permissionsIn(privateChat).has(1024)) {
+    message.reply('You do not have permission to use that command.');
+    return;
+  }
   if (args.join(' ') == 'open') {
     dc.state.get('portal').state = true;
-    hook.send('The Portal has been opened');
+    message.reply('The Portal has been opened');
     let mems = portal.members;
     mems.forEach(e => {
       e.voice.setChannel(private);
