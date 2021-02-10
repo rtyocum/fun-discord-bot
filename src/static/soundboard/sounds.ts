c661fc89ee564fea621ca7bb7007bec0d9dbcfa4
Ryan Yocum <rtyocum1@gmail.com>
2021-02-10 13:18:08 -0500

    Added Effect

--- src/static/soundboard/sounds.ts
+++ src/static/soundboard/sounds.ts
@@ -8,5 +8,10 @@ export default [
     name: "Sicko Mode",
     value: "2️⃣",
     file: "sickomode.opus"
+  },
+  {
+    name: "Coconut Mall",
+    value: "3️⃣",
+    file: "coconutmall.opus"
   }
 ]
